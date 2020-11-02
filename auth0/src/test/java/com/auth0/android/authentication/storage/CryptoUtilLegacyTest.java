ca1aa5b064c79e229f1f53dc57724b8f2d30ed6a
Morgan Lutz <morgan.lutz@gmail.com>
2020-11-02 09:59:46 -0800

    Finish migration to AndroidX

--- auth0/src/test/java/com/auth0/android/authentication/storage/CryptoUtilLegacyTest.java
+++ auth0/src/test/java/com/auth0/android/authentication/storage/CryptoUtilLegacyTest.java
@@ -2,7 +2,7 @@ package com.auth0.android.authentication.storage;
 
 import android.content.Context;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
+import androidx.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
